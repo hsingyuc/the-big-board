2a1a43197eb686f99e218033076c54250b10ab1b
hsingyuc <hsingyuc.7@gmail.com>
2022-03-05 12:47:24 -0500

    Add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App: React.FC = () => {
 	return (
-		<div>The big board</div>
+		<div className='stocks-container'>
+			<h1>The big b<img src="bullinsuit.png" alt="Bull in suit" width="20" height="20"/>ard</h1>
+			<h2>Stock Comparison</h2>
+			Search here
+			<div className='cards-container'>
+				Cards here
+			</div>
+		</div>  
 	);
-}
+};
 	
 export default App;
 	
