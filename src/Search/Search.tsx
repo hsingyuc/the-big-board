6a13c07bf1a56aca411ddcc437eb140ef32c8a25
hsingyuc <hsingyuc.7@gmail.com>
2022-03-05 21:46:20 -0500

    Close search dropdown on click outside

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Dropdown } from './Dropdown';
 import { Stock, StockResult } from '../utils/types';
 import { sanitizeProperties } from '../utils/utils';
@@ -6,12 +6,30 @@ import { sanitizeProperties } from '../utils/utils';
 export const Search: React.FC = ()  => {
 	const [results, setResults] = useState<Stock[]>( [] );
 	const [query, setQuery] = useState<string>( '' );
+	const [isOpen, setIsOpen] = useState<boolean>( false );
+	const selectBoxRef = useRef<HTMLInputElement>( null );
 	
 	useEffect( () => {
 		if ( query.length ) {
 			fetchStocks( query );
 		}
 	},[query] );
+
+	useEffect( () => {
+		if ( ! isOpen ) {
+			return;
+		}
+
+		const handleCurrentTarget = ( e: MouseEvent | TouchEvent ) => {
+			if ( ! selectBoxRef.current?.contains( e.target as Node ) ) {
+				setQuery( '' );
+				setIsOpen( false );
+			}
+		};
+
+		document.addEventListener( 'click', handleCurrentTarget );
+		return () => document.removeEventListener( 'click', handleCurrentTarget );
+	}, [isOpen] );
 
 	const fetchStocks = ( keywords:string ) => {
 		fetch( `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${process.env.REACT_APP_API_KEY}` )
@@ -21,12 +39,13 @@ export const Search: React.FC = ()  => {
 					return sanitizeProperties( item );
 				} );
 				setResults( results );
+				setIsOpen( true );
 			} )
 			.catch( error=>console.log( `Error in fetch: ${error.message}` ) );
 	};
-
+	
 	return ( 
-		<div className='search-container'>
+		<div className='search-container' ref={selectBoxRef}>
 			<div className='search-container__search'>
 				<input
 					className='search-container__search-box'
@@ -35,9 +54,9 @@ export const Search: React.FC = ()  => {
 					aria-label='Search stocks'
 					onChange={( event )=>setQuery( event.target.value )}
 				/> 
-				<Dropdown 
+				{ isOpen && <Dropdown 
 					options={results} 
-				/>
+				/>}
 			</div>
 		</div>
 	);
